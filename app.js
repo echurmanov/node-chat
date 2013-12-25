3db0fee4683fc2a43c7a531ea071cdfa2f4e5a97
echurmanov <eugene@soft-rose.com>
2013-12-25 14:35:24 +0600

    Переработана логи приложения

--- app.js
+++ app.js
@@ -15,19 +15,140 @@ server.on('request', function(req, resp){
     resp.end('');
 });
 
-function userObject(userName, credentials) {
-    this.userName = userName;
+function userObject(userId, credentials) {
+    this.userId = userId;
     this.credentials = credentials;
 
+    this.sockets = [];
+
     this.export = function() {
-        return {
-            userId: this.userName,
-            userData: this.credentials
+        return this.credentials;
+    };
+
+
+}
+
+
+function Chat (name, socketManager) {
+    this.socketManager = socketManager;
+    this.name = name;
+    this.users = {};
+    this.sockets = {};
+
+
+    this.kickUser = function (user, socket) {
+        if (typeof this.sockets[user.userId] !== 'undefined') {
+            if (this.sockets[user.userId].indexOf(socket) != -1) {
+                this.sockets[user.userId].splice(this.sockets[user.userId].indexOf(socket),1);
+                if (typeof this.sockets[user.userId] == 'undefined' || this.sockets[user.userId].length == 0) {
+                    var chat = this;
+                    var u = user;
+                    (function () {
+
+                    user.timeOut = setTimeout(function(){
+                        if (typeof chat.sockets[u.userId] !== 'undefined') {
+                            delete(chat.sockets[u.userId]);
+                        }
+                        if (typeof chat.users[u.userId] != 'undefined') {
+                            delete(chat.users[u.userId]);
+                            chat.socketManager.sockets.in(chat.name).emit('sys-message', {type: 'user-leave', user: u.export()});
+                            chat.socketManager.sockets.in(chat.name).emit('sys-message', {type: 'user-list', users: chat.exportUsers()});
+                        }
+                    }, 30000);
+                    })();
+                }
+            }
         }
+
+    };
+
+    this.joinUser = function(user, socket) {
+        socket.join(this.name);
+        socket.user = user;
+        if (typeof this.users[user.userId] == 'undefined') {
+            this.users[user.userId] = user;
+            this.socketManager.sockets.in(this.name).emit('sys-message', {type: 'user-join', user: user.export()});
+            this.socketManager.sockets.in(this.name).emit('sys-message', {type: 'user-list', users: this.exportUsers()});
+
+        } else {
+            if (typeof user.timeOut !== 'undefined') {
+                clearTimeout(user.timeOut);
+                delete(user.timeOut);
+            }
+        }
+        if (typeof this.sockets[user.userId] == 'undefined') {
+            this.sockets[user.userId] = [];
+        }
+        if (this.sockets[user.userId].indexOf(socket) == -1) {
+            this.sockets[user.userId].push(socket);
+        }
+
+
+        var chat = this;
+
+        socket.on('disconnect', function(sock){
+            if (typeof this.user !== 'undefined') {
+                var user = this.user;
+                chat.kickUser(user, this);
+            }
+        });
+
+
+        socket.on('leave', function kickUser(data){
+            this.leave(data.chatName);
+            if (data.chatName == chat.name) {
+                chat.kickUser(this.user, this);
+            }
+        });
+        socket.on('get-user-list', function(data){
+            if (data.chatName == chat.name) {
+                this.emit('sys-message', {type: 'user-list', users: chat.exportUsers()});
+            }
+        });
+
+        socket.on('message', function sendMessage(data){
+            var text = data.message;
+
+            var replaces = {
+                "<": "&lt;",
+                ">": "&gt;",
+                "'": "&#39;",
+                "\"": "&quot;"
+            };
+            for (var p in replaces) {
+                text = text.split(p).join(replaces[p]);
+            }
+
+            var message = {
+                user:  this.user.export(),
+                message: text
+
+            };
+
+            if (chat.name == data.chatName) {
+                socket.manager.sockets.in(data.chatName).emit('message', message);
+            }
+        });
+
+    };
+
+    this.getUser = function (userId) {
+        if (typeof this.users[userId] !== 'undefined') {
+            return this.users[userId];
+        }
+        return null;
+    };
+
+    this.exportUsers = function () {
+        var data = [];
+        for (var userIndex in this.users) {
+            data.push(this.users[userIndex].credentials);
+        }
+        return data;
     }
 }
 
-var USERS = {};
+var CHATS = {};
 
 var socketServer = io.listen(server);
 
@@ -36,66 +157,26 @@ socketServer.configure(function configureSocketIO(){
     socketServer.enable('browser client expiration');  // send minified client
     socketServer.enable('browser client etag');          // apply etag caching logic based on version number
     socketServer.enable('browser client gzip');          // gzip the file
-    //socketServer.set('log level', 1);                    // reduce logging
+    socketServer.set('log level', 1);                    // reduce logging
 });
 
 socketServer.sockets.on('connection', function (socket) {
     console.log("Connection");
     socket.on('join', function joinUser(data){
-        var newUser = null;
-        var n = true;
-        if (typeof USERS[data.userId] !== 'undefined') {
-            newUser = USERS[data.userId];
-            if (typeof newUser.timeOut !== 'undefined') {
-                clearTimeout(newUser.timeOut);
-                delete(newUser.timeOut);
-            }
-            n = false;
-        } else {
-            newUser = new userObject(data.userId, data.userData);
-        }
-        USERS[newUser.userName] = newUser;
-        this.join(data.chatName);
-        this.user = newUser;
-        if (n) {
-            this.manager.sockets.in(data.chatName).emit('sys-message', {type: 'user-join', user: this.user.export()});
+
+
+        if (typeof CHATS[data.chatName] == 'undefined') {
+            CHATS[data.chatName] = new Chat(data.chatName, this.manager);
         }
 
-    });
-
-    socket.on('leave', function kickUser(data){
-        this.leave(data.chatName);
-        this.manager.sockets.in(data.chatName).emit('sys-message', {type: 'user-leave', user: this.user.export()});
-    });
-
-    socket.on('message', function sendMessage(data){
-        var text = data.message;
-
-        var replaces = {
-            "<": "&lt;",
-            ">": "&gt;",
-            "'": "&#39;",
-            "\"": "&quot;"
-        };
-        for (var p in replaces) {
-            text = text.split(p).join(replaces[p]);
+        var user = CHATS[data.chatName].getUser(data.userId);
+        if (user == null) {
+            user = new userObject(data.userId, data.userData);
         }
-
-        var message = {
-            user:  this.user.export(),
-            message: text
-
-        };
-        socket.manager.sockets.in(data.chatName).emit('message', message);
+        CHATS[data.chatName].joinUser(user, this);
     });
-    socket.on('disconnect', function(sock){
-        var user = this.user;
-        if (typeof this.user !== 'undefined') {
-            this.user.timeOut = setTimeout(function(){
-                delete(USERS[user.userName]);
-            }, 30000);
 
-            delete(this.user);
-        }
-    });
+
+
+
 });
--- public/chat.js
+++ public/chat.js
@@ -18,11 +18,15 @@ if (typeof window.Chat === 'undefined') {
         };
 
         this.join = function(chatname) {
-            this.socket.emit('join', {userId: user.userId, userData: this.userData, chatname: chatname});
+            this.socket.emit('join', {userId: this.user.userId, userData: this.user.userData, chatName: chatname});
+        };
+
+        this.getList = function(chatname) {
+            this.socket.emit('get-user-list', {chatName: chatname});
         };
 
         this.message = function (chatname, message) {
-            this.socket.emit('message', {chatname: chatname, message: message});
+            this.socket.emit('message', {chatName: chatname, message: message});
         };
 
         var chat = this;
@@ -32,7 +36,7 @@ if (typeof window.Chat === 'undefined') {
             }
         });
 
-        this.socket.on('message', function(data) {
+        this.socket.on('sys-message', function(data) {
             if (typeof chat._event['onsysmessage'] === 'function') {
                 chat._event['onsysmessage'](data);
             }
